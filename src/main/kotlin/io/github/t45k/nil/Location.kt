8c2a02ce71a1f3fb6a14e45ea9428b6b91760406
T45K <tasktas9@gmail.com>
2020-10-24 17:36:27 +0900

    map

--- src/main/kotlin/io/github/t45k/nil/Location.kt
+++ src/main/kotlin/io/github/t45k/nil/Location.kt
@@ -9,16 +9,11 @@ class Location(private val filteringThreshold: Int, private val codeBlocks: List
     private val hashTable: MutableMap<Int, MutableList<Int>> = HashMap(500_000)
 
     fun locate(tokenSequence: TokenSequence): List<Int> =
-        tokenSequence
-            .flatMap { hashTable[it] ?: emptyList() }
-            .groupingBy { it }
-            .eachCount()
-            .filter {
-                it.value * 100 / min(
-                    tokenSequence.size,
-                    codeBlocks[it.key].tokenSequence.size
-                ) >= filteringThreshold
-            }
+        mutableMapOf<Int, Int>().apply {
+            tokenSequence.flatMap { hashTable[it] ?: emptyList() }
+                .forEach { compute(it) { _, v -> if (v == null) 1 else v + 1 } }
+        }
+            .filter { it.value * 100 / min(tokenSequence.size, codeBlocks[it.key].tokenSequence.size) >= 10 }
             .keys
             .toList()
 
