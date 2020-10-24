82d01579f2c58f39d4f2c84d859d8f6c33aa68b2
T45K <tasktas9@gmail.com>
2020-10-24 22:09:31 +0900

    add option

--- src/main/kotlin/io/github/t45k/nil/NILConfig.kt
+++ src/main/kotlin/io/github/t45k/nil/NILConfig.kt
@@ -7,6 +7,7 @@ data class NILConfig(
     val minLine: Int = 6,
     val minToken: Int = 50,
     val gramSize: Int = 5,
+    val partitionSize: Int = 500_000,
     val filteringThreshold: Int = 10,
     val verifyingThreshold: Int = 70,
     val outputFileName: String = "result.csv",
@@ -17,21 +18,23 @@ fun parseArgs(args: Array<String>): NILConfig {
     var minLine = 6
     var minToken = 50
     var gramSize = 5
+    var partitionSize = 500_000
     var filteringThreshold = 10
     var verifyingThreshold = 70
     var outputFileName: String? = null
 
     val iterator = args.iterator()
     while (iterator.hasNext()) {
-        when (iterator.next()) {
+        when (val optionName = iterator.next()) {
             "-s", "--src" -> src = File(iterator.next())
             "-mil", "--min-line" -> minLine = iterator.next().toInt()
             "-mit", "--min-token" -> minToken = iterator.next().toInt()
             "-g", "--gram-size" -> gramSize = iterator.next().toInt()
+            "-p", "--partition-size" -> partitionSize = iterator.next().toInt()
             "-f", "--filtering-threshold" -> filteringThreshold = iterator.next().toInt()
             "-v", "--verifying-threshold" -> verifyingThreshold = iterator.next().toInt()
             "-o", "--output" -> outputFileName = iterator.next()
-            else -> throw RuntimeException("Invalid option")
+            else -> throw InvalidOptionException(optionName)
         }
     }
 
@@ -40,8 +43,23 @@ fun parseArgs(args: Array<String>): NILConfig {
         minLine,
         minToken,
         gramSize,
+        partitionSize,
         filteringThreshold,
         verifyingThreshold,
         outputFileName ?: "result_${gramSize}_${filteringThreshold}.csv",
     )
+}
+
+class InvalidOptionException(private val option: String) : RuntimeException() {
+    override val message: String?
+        get() = """$option is invalid option.
+            |-s, --src${'\t'}Source directory
+            |-mil, --min-line${'\t'}Minimum line
+            |-mit, --min-token${'\t'}Minimum token
+            |-g, --gram-size${'\t'}N of N-gram
+            |-p, --partition-size${'\t'}Size of partition
+            |-f, --filtering-threshold${'\t'}Filtering threshold
+            |-v, --verifying-threshold${'\t'}Verifying threshold
+            |-o, --output${'\t'}Output file name
+        """.trimMargin()
 }
