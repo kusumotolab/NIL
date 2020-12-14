d5ae71949885573d1f866e20e39371a9d062c331
T45K <tasktas9@gmail.com>
2020-12-14 16:21:40 +0900

    Revert "debug"
    This reverts commit 3ec0ad3e0d2e3e7b246755c52ef73d1fdd2f0e9c.

--- src/main/kotlin/io/github/t45k/nil/NILMain.kt
+++ src/main/kotlin/io/github/t45k/nil/NILMain.kt
@@ -50,12 +50,10 @@ class NILMain(private val config: NILConfig) {
                                 .toObservable()
                                 .filter { index > it }
                                 .filter { verification.verify(index, it) }
-                                .doOnEach { println(Thread.currentThread().toString() + " clone") }
                                 .map { it to index }
                         }
                 }
                 .blockingSubscribe {
-                    println(Thread.currentThread().toString() + " write")
                     bufferedWriter.appendLine("${reformat(codeBlocks[it.first])},${reformat(codeBlocks[it.second])}")
                 }
         }
