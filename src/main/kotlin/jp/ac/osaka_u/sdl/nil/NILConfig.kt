5e95980ecbd42dd714fec472e28ffc66de0cafe7
T45K <tasktas9@gmail.com>
2021-02-12 14:09:40 +0900

    add description

--- src/main/kotlin/jp/ac/osaka_u/sdl/nil/NILConfig.kt
+++ src/main/kotlin/jp/ac/osaka_u/sdl/nil/NILConfig.kt
@@ -98,6 +98,7 @@ class InvalidOptionException(private val option: String) : RuntimeException() {
             |-v, --verification-threshold${'\t'}${'\t'}Verification threshold (default: 70%)
             |-o, --output${'\t'}${'\t'}${'\t'}${'\t'}Output file name (default: result_{N-gram}_{filtering_threshold}_{verifying_threshold}.csv)
             |-t, --threads${'\t'}${'\t'}${'\t'}${'\t'}The number of threads used for parallel execution (default: all threads)
+            |-l, --language${'\t'}${'\t'}${'\t'}${'\t'}Target language (default: java)
             |-bce, --bigcloneeval${'\t'}${'\t'}${'\t'}Output result feasible to BigCloneEval (default: false)
             |-mif, --mutationinjectionframework${'\t'}Output result feasible to MutationInjectionFramework (default: false)
         """.trimMargin()
