bfcf27c39b8f9c2606b957f892d9550dbb7aa7b5
T45K <tasktas9@gmail.com>
2024-06-09 12:38:28 +0900

    build

--- src/main/kotlin/jp/ac/osaka_u/sdl/nil/util/RxExtention.kt
+++ src/main/kotlin/jp/ac/osaka_u/sdl/nil/util/RxExtention.kt
@@ -3,7 +3,7 @@ package jp.ac.osaka_u.sdl.nil.util
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.parallel.ParallelFlowable
 
-fun <T> Flowable<T>.parallelIfSpecified(threads: Int): ParallelFlowable<T> =
+fun <T : Any> Flowable<T>.parallelIfSpecified(threads: Int): ParallelFlowable<T> =
     if (threads > 0) {
         parallel(threads)
     } else {
